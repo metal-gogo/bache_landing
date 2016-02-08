b7c4a723d432de61c1d05b7a0057a768943a8b94
gogo-koko <daniel@kokonutstudio.com>
2016-02-07 20:17:07 -0600

    Git ignore created basic HTML markup created

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: metal-gogo
+ * Date: 2/7/2016
+ * Time: 8:06 PM
+ */
+date_default_timezone_set( 'America/Mexico_City' );
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Bache 24</title>
+</head>
+<body>
+
+</body>
+</html>
+
