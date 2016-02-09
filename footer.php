8003ced2d67e5d19847fab644fac69dc698c6f19
gogo-koko <daniel@kokonutstudio.com>
2016-02-09 11:29:03 -0600

    Se dejó preparado el footer del sitio

--- footer.php
+++ footer.php
@@ -6,4 +6,9 @@
  * Time: 10:43 PM
  */
 ?>
-
+<footer class="section-container footer-container">
+	<div class="footer-text"><a class="footer-link" href="">Desarrollado por CMI</a></div>
+	<div class="footer-text"><a class="footer-link" href="">Aviso de privacidad</a></div>
+	<div class="footer-text">Ciudad de México Derechos reservados <?php echo date("Y") ?></div>
+	<div style="clear: both;"></div>
+</footer>
