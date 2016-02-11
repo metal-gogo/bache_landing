fd86fb32f6934b5e084ddbcb00833827f8383306
gogo-koko <daniel@kokonutstudio.com>
2016-02-11 17:53:15 -0600

    Se agregó el texto real del aviso de privacidad

--- texto.aviso.php
+++ texto.aviso.php
@@ -8,10 +8,337 @@
 ?>
 
 <section id="section-privacidad" class="section section-privacidad">
-	<h1 class="privacy-title">Aviso de privacidad</h1>
-	<p class="privacy-text">Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad.</p>
-	<p class="privacy-text">Ejemplo de texto de aviso de privacidad.</p>
-	<p class="privacy-text">Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad.</p>
-	<p class="privacy-text">Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad.</p>
-	<p class="privacy-text">Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad. Ejemplo de texto de aviso de privacidad.</p>
+	<h1 class="privacy-title">Aviso de privacidad integral</h1>
+	<p class="privacy-text">
+		Su privacidad y confianza son muy importantes para nosotros. Por ello, queremos asegurarnos de que conozca
+		como salvaguardamos la integridad, privacidad y protección de sus Datos Personales.
+	</p>
+	<p class="privacy-text">
+		Consideramos que por ser una Entidad de la Administración Pública del Distrito Federal, con facultades y
+		también responsabilidades tenemos la obligación legal y social de cumplir con las medidas legales y
+		de seguridad suficientes para proteger aquellos Datos Personales que se hayan recabado para las
+		finalidades que se describen en el presente aviso de privacidad.
+	</p>
+	<p class="privacy-text">
+		Se da a conocer el presente aviso de privacidad en estricto acatamiento a los artículos tercero transitorio,
+		16 y 17 de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, publicada en el
+		Diario Oficial de la Federación el día 5 de julio del año 2010, ordenamiento legal que tiene por objeto la
+		protección de los datos personales en posesión de los particulares, con la finalidad de regular su tratamiento
+		legítimo, controlado e informado, a efecto de garantizar la privacidad y el derecho a la autodeterminación
+		informativa de las personas, así como de su Reglamento, publicado en el Diario Oficial de la Federación
+		el 21 de diciembre de 2011 y los Lineamientos del Aviso de Privacidad, publicados en el mismo medio
+		informativo el 17 de enero de 2013. Derivado de lo antes expuesto, Usted tendrá pleno control y
+		decisión sobre sus Datos Personales, por ello, le recomendamos leer atentamente la siguiente información.
+	</p>
+	<ol>
+		<!-- Definiciones -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Definiciones.</h2>
+			<p class="privacy-text">
+				De acuerdo con la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, su
+				Reglamento, los Lineamientos del Aviso de Privacidad y con el presente aviso, se entenderá por:
+			</p>
+			<ol>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Datos personales:
+					</h3>
+					<p class="definition-text">
+						Cualquier información concerniente a una persona física identificada o identificable.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Datos personales sensibles:
+					</h3>
+					<p class="definition-text">
+						Aquellos datos personales que afecten a la esfera más íntima de su titular, o cuya utilización
+						indebida pueda dar origen a discriminación o conlleve un riesgo grave para éste. En particular,
+						se consideran sensibles aquellos que puedan revelar aspectos como origen racial, étnico, estado
+						de salud presente y futura, información genética, creencias religiosas, filosóficas y morales,
+						afiliación sindical, opiniones políticas, preferencia sexual.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Derechos ARCO:
+					</h3>
+					<p class="definition-text">
+						Derechos de acceso, rectificación, cancelación y oposición previstos en la Ley Federal de
+						Protección de Datos Personales en Posesión de los Particulares.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Ley:
+					</h3>
+					<p class="definition-text">
+						Ley Federal de Protección de Datos Personales en Posesión de los Particulares.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Lineamientos:
+					</h3>
+					<p class="definition-text">
+						Lineamientos del Aviso de Privacidad.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Reglamento:
+					</h3>
+					<p class="definition-text">
+						Reglamento de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Remisión:
+					</h3>
+					<p class="definition-text">
+						La comunicación de datos personales entre el responsable y el encargado, dentro o fuera
+						del territorio mexicano.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Responsable:
+					</h3>
+					<p class="definition-text">
+						El Gobierno de la Ciudad de México, La Dirección General de Servicios Urbanos y las Direcciones
+						y Subdirecciones de Área que conforman la misma y las cuales se verán involucradas en la
+						implementación del programa “BACHE 24”, mencionando a la Dirección de Mantenimiento de
+						Infraestructura Urbana, la Subdirección de Mantenimiento e Infraestructura Vial y la
+						Subdirección de Mobiliario Urbano, que llevaran a cabo el tratamiento de sus datos personales.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+				<li class="privacy-sublist">
+					<h3 class="definition-title">
+						Ttular:
+					</h3>
+					<p class="definition-text">
+						La persona física a quien corresponden los datos personales.
+					</p>
+					<div style="clear: both;"></div>
+				</li>
+			</ol>
+		</li>
+		<!-- Identidad y domicilio del responsable -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Identidad y domicilio del responsable.</h2>
+			<p class="privacy-text">
+				El presente Aviso de Privacidad (en lo sucesivo referido como “Aviso”) establece los términos y
+				condiciones en virtud de los cuales el Gobierno de la Ciudad de México a través de la Dirección
+				General de Servicios Urbanos de la Secretaría de Obras y Servicios (en adelante “LA DGSU”), en
+				conjunto con las Direcciones y Subdirecciones de Área que la conforman, con domicilio para oír y
+				recibir notificaciones en Avenida Río Churubusco, número 1155, Colonia Licenciado Carlos Zapata
+				Vela, Delegación Iztacalco, C.P. 08040, en esta Ciudad de México; en su carácter de Responsable
+				(o el Encargado que designe), serán los Responsables del almacenamiento, uso, manejo,
+				aprovechamiento y disposición de los datos personales que recabe de los Ciudadanos y que se
+				encuentren ligados a la aplicación electrónica denominada “BACHE 24”.
+			</p>
+		</li>
+		<!-- Consentimiento del titular -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Consentimiento del titular.</h2>
+			<p class="privacy-text">
+				Para efectos de lo dispuesto en la Ley Federal de Protección de Datos Personales en Posesión de los
+				Particulares (la “LFPDP”) y demás legislación aplicable, el Titular manifiesta:
+			</p>
+			<ol>
+				<li>
+					<p class="privacy-text">
+						Que el presente Aviso le ha sido dado a conocer por el Responsable.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Que otorga su consentimiento respecto del tratamiento de sus datos personales, para todo
+						lo relacionado con la ejecución de la aplicación electrónica denominada “BACHE 24”.
+					</p>
+				</li>
+			</ol>
+		</li>
+		<!-- Información proporcionada por el titular -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Información proporcionada por el titular.</h2>
+			<p class="privacy-text">
+				El Gobierno de la Ciudad de México recolectara los datos personales de manera indirecta a través de
+				la aplicación de acceso público electrónica denominada “BACHE 24” y, recabara entre otros los
+				siguientes datos:
+			</p>
+			<ul>
+				<li>
+					<p class="privacy-text">
+						Nombre completo.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Clave Única de Registro de Población.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Fecha de nacimiento.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Nacionalidad.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Registro Federal de Contribuyentes.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Sexo y Estado Civil.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Correo electrónico.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Domicilio y Número de Teléfono.
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Ocupación.
+					</p>
+				</li>
+			</ul>
+		</li>
+		<!-- Política de datos sensibles -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Política de datos sensibles.</h2>
+			<p class="privacy-text">
+				El responsable dentro de sus políticas comerciales no solicita ninguna información que bajo la Ley
+				se entiende como datos sensibles, tales como religión, preferencias sexuales, opiniones políticas,
+				datos financieros y/o patrimoniales y, toda vez que el Gobierno de la Ciudad de México fomenta la
+				diversidad, prohíbe cualquier acto discriminatorio o la solicitud de cualquier información que
+				pudiese implicar un acto discriminatorio.
+			</p>
+		</li>
+		<!-- Finalidad de los datos personales -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Finalidad de los datos personales.</h2>
+			<p class="privacy-text">
+				Los datos personales que recaba el Gobierno de la Ciudad de México, serán protegidos, incorporados
+				y tratados en la aplicación electrónica denominada “BACHE 24” y, tienen como finalidad capturar los
+				reportes y solicitudes de la ciudadanía relacionadas con la funcionalidad de la vía pública y
+				movilidad, así como el deterioro del pavimento, asignar un número de folio e identificar al
+				peticionario para dar seguimiento y atención a su solicitud.
+			</p>
+			<p class="privacy-text">
+				El tratamiento de los Datos Personales proporcionados por el Titular al Responsable se limitará
+				al cumplimiento de las finalidades previstas en el presente aviso de privacidad y a fines distintos
+				que resulten compatibles o análogos a los establecidos en dicho aviso, sin que para ello se
+				requiera obtener nuevamente el consentimiento del Titular.
+			</p>
+			<p class="privacy-text">
+				El Titular manifiesta que los Datos Personales proporcionados al Responsable son veraces y se
+				hace responsable de comunicar a éste cualquier modificación a los mismos a través de los
+				formatos designados para ello.
+			</p>
+		</li>
+		<!-- Video y fotografía -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Video y fotografía.</h2>
+			<p class="privacy-text">
+				El Gobierno de la Ciudad de México, no será responsable en caso de que el Ciudadano al momento de
+				captar la fotografía o video grabar el bache, no tome las medidas precautorias necesarias y
+				suficientes para la obtención de las mismas, no responsabilizándose además por las infracciones
+				de tránsito a que se pueda hacer acreedor por el mal uso de medios electrónicos.
+			</p>
+			<p class="privacy-text">
+				Para el caso de que el Titular remita a través de la aplicación electrónica denominada “BACHE 24”
+				información falsa respecto de la vialidad a reportar, la solicitud no será procedente, dándose por
+				concluido el folio generado.
+			</p>
+		</li>
+		<!-- Procedimiento para ejercer los derechos arco y revocación del consentimiento -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Procedimiento para ejercer los derechos arco y revocación del consentimiento.</h2>
+			<p class="privacy-text">
+				El Titular tiene, en todo momento, derecho de ACCEDER, RECTIFICAR y cancelar sus datos, así como
+				de OPONERSE al tratamiento de los mismos o REVOCAR el consentimiento que haya proporcionado
+				presentando una solicitud en el formato que para tal fin le entregaremos a petición expresa,
+				misma que debe contener la información y documentación siguiente:
+			</p>
+			<ol>
+				<li>
+					<p class="privacy-text">
+						Nombre del Titular y domicilio u otro medio para comunicarle la respuesta a su solicitud;
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Los documentos vigentes que acrediten su identidad (copia simple en formato impreso o
+						electrónico de su credencial de elector, pasaporte o Visa) o, en su caso, la representación
+						legal del Titular (copia simple en formato impreso o electrónico de la carta poder simple
+						con firma autógrafa del Titular, el mandatario y sus correspondientes identificaciones
+						oficiales vigentes – credencial de elector, pasaporte o Visa);
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						La descripción clara y precisa de los datos respecto de los que busca ejercer alguno
+						de los Derechos ARCO, y
+					</p>
+				</li>
+				<li>
+					<p class="privacy-text">
+						Cualquier otro elemento o documento que facilite la localización de los
+						datos personales del Titular.
+					</p>
+				</li>
+			</ol>
+			<p class="privacy-text">
+				El Gobierno de la Ciudad de México, responderá al Titular en un plazo máximo de veinte días
+				hábiles, contados desde la fecha en que se recibió la solicitud a efecto de que, si resulta
+				procedente, haga efectiva la misma dentro de los quince días hábiles siguientes a que se
+				comunique la respuesta. En todos los casos, la respuesta se dará por la misma vía por la que
+				haya presentado su solicitud o en su caso por cualquier otro medio acordado con el Titular.
+				Los plazos antes referidos podrán ser ampliados en términos de la LFPDP.
+			</p>
+		</li>
+		<!-- Cambios al aviso -->
+		<li class="privacy-list">
+			<h2 class="privacy-subtitle">Cambios al aviso.</h2>
+			<p class="privacy-text">
+				El Gobierno de la Ciudad de México, se reserva el derecho de actualizar periódicamente el
+				presente Aviso para reflejar los cambios en sus prácticas de información. Es responsabilidad del
+				Titular revisar el contenido del Aviso de privacidad. El Responsable entenderá que de no expresar
+				lo contrario, significa que el Titular ha leído, entendido y acordado los términos ahí expuestos,
+				lo que constituye su consentimiento a los cambios y/o actualizaciones respecto al tratamiento de
+				sus datos personales.
+			</p>
+			<p class="privacy-text">
+				Así mismo, en términos del párrafo segundo, del artículo 14 del Reglamento y al artículo Vigésimo
+				quinto de los Lineamientos, se informa a los Titulares de los Datos Personales que tendrán un
+				plazo de cinco días hábiles, contados a partir del día siguiente a que se hayan registrado en la
+				aplicación electrónica denominada “BACHE 24”, para que, de ser el caso, manifiesten su negativa
+				para el tratamiento de sus Datos Personales con respecto a las finalidades descritas en el
+				presente aviso de privacidad que no sean necesarias, ni dieron origen a la relación jurídica
+				con el Responsable a través de una solicitud a dicha negativa a la dirección de correo electrónico
+				<a href="mailto:efrancoo@df.gob.mx">efrancoo@df.gob.mx</a>.
+			</p>
+		</li>
+	</ol>
 </section>
